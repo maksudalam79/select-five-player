5c0fb85a728fae5fbe9af9f970396d0df9a6672e
maksudalam <maksud.cdz@gmail.com>
2022-08-22 15:38:46 +0600

    player-added-total

--- Select/select.js
+++ Select/select.js
@@ -1,1 +1,9 @@
-console.log('clicked')
+const cardArray = [];
+function addToCard(element) {
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    const playerObj = {
+        playerName: playerName
+    }
+    cardArray.push(playerObj)
+    document.getElementById('total-added-card').innerText = cardArray.length;
+}
