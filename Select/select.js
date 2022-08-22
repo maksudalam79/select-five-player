b682657749ffed02c135fd44a5b9a40707bdeda5
maksudalam <maksud.cdz@gmail.com>
2022-08-23 01:32:38 +0600

    calculate total

--- Select/select.js
+++ Select/select.js
@@ -55,10 +55,53 @@ document.getElementById('per-person').addEventListener('click', function () {
     const totalPlayerString = totalPlayerExpenses.innerText;
     const totalPlayerNumber = parseInt(totalPlayerString);
     totalPlayerExpenses.innerText = totalPerson;
-
     return totalPlayerNumber;
 
+
 })
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+
+    const playerAmount = document.getElementById('player-amount');
+    const playerString = playerAmount.value;
+    const playerNumber = parseInt(playerString);
+
+    const totalAddedCard = document.getElementById('total-added-card');
+    const totalAddedCardstring = totalAddedCard.innerText;
+    const totalAddedCardNumber = parseInt(totalAddedCardstring);
+
+    const totalPerson = playerNumber * totalAddedCardNumber;
+
+
+    const totalPlayerExpenses = document.getElementById('total-player-expenses');
+    const totalPlayerString = totalPlayerExpenses.innerText;
+    const totalPlayerNumber = parseInt(totalPlayerString);
+    totalPlayerExpenses.innerText = totalPerson;
+
+
+
+    const managerAmmount = document.getElementById('Manager-ammount');
+    const managerString = managerAmmount.value;
+    const managerNumber = parseInt(managerString);
+
+    const coachAmmount = document.getElementById('coach-ammount');
+    const coachString = coachAmmount.value;
+    const coachNumber = parseInt(coachString);
+
+    const total = document.getElementById('total');
+    const totalString = total.innerText;
+    const totalNumber = parseInt(totalString);
+
+    const totalTaka = totalPlayerNumber + managerNumber + coachNumber;
+
+    total.innerText = totalTaka;
+
+    return (totalNumber, totalTaka)
+
+})
+
+
+
 
 
 
